e1fe61d32cb40ce81b849be39bca3b1275fea0f6
waxx6996 <136496276+waxx6996@users.noreply.github.com>
2024-01-12 02:30:19 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Next.js waxx takeover</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
